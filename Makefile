6510189aec709c0e0c99f7adb5930f99e2c417ec
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-05-18 16:43:26 +0200

    Wip on postgres connection

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ dev:
 	HUMBLE_ENV=dev humble up -d
 	HUMBLE_ENV=dev humble logs -f
 
+undev:
+	HUMBLE_ENV=dev humble down
+
 dev-build:
 	HUMBLE_ENV=dev humble exec webapp yarn build
 
@@ -10,3 +13,5 @@ prod:
 	HUMBLE_ENV=prod humble up -d --build
 	HUMBLE_ENV=prod humble logs -f
 	
+unprod:
+	HUMBLE_ENV=prod humble down
--- services/webapp/ssr/boot.js
+++ services/webapp/ssr/boot.js
@@ -1,15 +1,21 @@
 import { registerAction, SETTINGS, FINISH } from '@marcopeg/hooks'
 import { createHookApp, logBoot } from '@marcopeg/hooks'
+import * as config from '@marcopeg/utils/lib/config'
 
 // polyfill "fetch" in NodeJS
 require('es6-promise').polyfill()
 require('isomorphic-fetch')
 
-const features = []
+const features = [
+    require('./features/auth'),
+]
 
 const services = [
     require('@forrestjs/service-env'),
     require('@forrestjs/service-logger'),
+    require('@forrestjs/service-jwt'),
+    require('@forrestjs/service-hash'),
+    require('@forrestjs/service-postgres'),
     require('@forrestjs/service-express'),
     require('@forrestjs/service-express-cookies'),
     require('@forrestjs/service-express-graphql'),
@@ -21,12 +27,33 @@ registerAction({
     hook: SETTINGS,
     name: '♦ boot',
     handler: async ({ settings }) => {
+        settings.postgres = [{
+            connectionName: 'default',
+            host: config.get('PG_HOST'),
+            port: config.get('PG_PORT'),
+            database: config.get('PG_DATABASE'),
+            username: config.get('PG_USERNAME'),
+            password: config.get('PG_PASSWORD'),
+            maxAttempts: Number(config.get('PG_MAX_CONN_ATTEMPTS', 25)),
+            attemptDelay: Number(config.get('PG_CONN_ATTEMPTS_DELAY', 5000)),
+            models: [],
+        }]
+
         settings.express = {
             ssr: {
                 // multilanguage cache policy
                 shouldCache: (req) => (req.query.locale === undefined),
                 getCacheKey: (req) => ({ value: [ req.url, req.locale.language, req.locale.region ] }),
             },
+        }
+
+        settings.jwt = {
+            secret: config.get('JWT_SECRET'),
+            duration: config.get('JWT_DURATION'),
+        }
+
+        settings.hash = {
+            rounds: Number(config.get('BCRYPT_ROUNDS')),
         }
     },
 })
